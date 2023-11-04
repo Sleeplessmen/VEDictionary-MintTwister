e3458fe8fb27aec90b223273d3ecc239469b8789
cusnaruto <hoangvip3333@gmail.com>
2023-11-04 17:36:56 +0700

    changeable themes

--- src/main/settingController.java
+++ src/main/settingController.java
@@ -1,10 +1,15 @@
 
+import atlantafx.base.theme.Dracula;
+import atlantafx.base.theme.NordDark;
+import atlantafx.base.theme.PrimerDark;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -16,11 +21,38 @@ public class settingController {
     private Scene scene;
     private Parent root;
 
-    public void switchToScene1(ActionEvent event) throws IOException {
+    @FXML
+    private ToggleGroup Themes;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private RadioButton themeDracula;
+
+    @FXML
+    private RadioButton themeNordDark;
+
+    @FXML
+    private RadioButton themePrimierDark;
+
+    public void switchToMainMenu(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    void getTheme(ActionEvent event) {
+        if(themeDracula.isSelected()) {
+            Application.setUserAgentStylesheet(new Dracula().getUserAgentStylesheet());
+        }
+        else if(themeNordDark.isSelected()) {
+            Application.setUserAgentStylesheet(new NordDark().getUserAgentStylesheet());
+        }
+        else if(themePrimierDark.isSelected()) {
+            Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
+        }
     }
 }
