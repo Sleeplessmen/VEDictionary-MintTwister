f31e61e04a0cf92be5b26e9c8b32a1a2a9566162
cusnaruto <hoangvip3333@gmail.com>
2023-11-06 03:05:10 +0700

    update read file

--- src/base/Word.java
+++ src/base/Word.java
@@ -1,18 +1,29 @@
-package src.base;
-
 public class Word {
     private String word_target;
     private String word_explain;
 
+    private String word_pronunciation;
     public Word() {}
 
-    public Word(String wt, String we) {
+    public Word(String wt, String we, String pr) {
         word_target = wt;
         word_explain = we;
+        word_pronunciation = pr;
+    }
+
+    public Word(String wordTarget, String[] wordExplains) {
     }
 
     public void setWord_target(String wt) {
         word_target = wt;
+    }
+
+    public String getWord_pronunciation() {
+        return word_pronunciation;
+    }
+
+    public void setWord_pronounciation(String word_pronunciation) {
+        this.word_pronunciation = word_pronunciation;
     }
 
     public void setWord_explain(String we) {
