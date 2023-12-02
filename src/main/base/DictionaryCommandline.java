e695dcb82e1d341d8fdad00800051859881009d9
cusnaruto <119999945+cusnaruto@users.noreply.github.com>
2023-12-03 00:26:36 +0700

    Update DictionaryCommandline.java

--- src/main/base/DictionaryCommandline.java
+++ src/main/base/DictionaryCommandline.java
@@ -1,115 +1,324 @@
 package base;
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
-public class DictionaryCommandline {
-    DictionaryManagement myDictionaryMng = new DictionaryManagement();
+public class DictionaryManagement extends Dictionary {
+    private static final String INPUTFILEPATH = "src/resources/FirstverDictionary.txt";
+    private static final String OUTPUTFILEPATH = "src/resources/OutputDictionaryFile.txt";
+    private static final String fileName = "src/resources/anhviet109K.txt";
 
-    public void dictionaryBasic() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n*****Dictionary Basic*****");
-        System.out.println("0: Exit");
-        System.out.println("1: Add words from commandline");
-        System.out.println("2: Show all words");
-        System.out.print("Your action: ");
 
-        char query = scanner.next().charAt(0);
-        switch (query) {
-            case '0':
-                System.out.println("Closing Dictionary Basic Version...");
-                return;
-            case '1':
-                myDictionaryMng.addWord();
-                break;
-            case '2':
-                myDictionaryMng.showAllWords();
-                break;
-            default:
-                System.out.println("Action not supported.");
-                break;
+    public void showAllWords() {
+        int counter = 1;
+        System.out.printf("%-5s| %-15s| %-15s| %-15s%n", "No", "English", "Pronunciation", "Vietnamese");
+
+        for (Word word : listWord) {
+            System.out.printf("%-5s| %-15s| %-15s| %-15s%n", counter, word.getWordTarget(), word.getWordPronunciation(), word.getWordExplain());
+            counter++;
         }
-        dictionaryBasic();
     }
 
-    public void dictionaryAdvanced() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n*****Dictionary Advanced*****!");
-        System.out.println("[0] Exit");
-        System.out.println("[1] Add");
-        System.out.println("[2] Remove");
-        System.out.println("[3] Modify");
-        System.out.println("[4] Display");
-        System.out.println("[5] Lookup");
-        System.out.println("[6] Search");
-        System.out.println("[7] Multiple-Choice Game");
-        System.out.println("[8] Import dictionary from file");
-        System.out.println("[9] Export dictionary to file");
-        System.out.print("Your action: ");
-
-        char query = scanner.next().charAt(0);
-        switch (query) {
-            case '0':
-                System.out.println("Closing Dictionary Advanced Version...");
-                return;
-            case '1':
-                myDictionaryMng.addWord();
-                break;
-            case '2':
-                myDictionaryMng.removeWord();
-                break;
-            case '3':
-                myDictionaryMng.modifyWord();
-                break;
-            case '4':
-                myDictionaryMng.showAllWords();
-                break;
-            case '5':
-                myDictionaryMng.dictionaryLookup();
-                break;
-            case '6':
-                myDictionaryMng.dictionarySearcher();
-                break;
-            case '7':
-                myDictionaryMng.mcq();
-                break;
-            case '8':
-                myDictionaryMng.insertWordFromFile();
-                break;
-            case '9':
-                myDictionaryMng.exportToFile();
-                break;
-            default:
-                System.out.println("Action not supported.");
-                break;
-        }
-        dictionaryAdvanced();
+    public void showOneWord(Word word) {
+        String wordTarget = word.getWordTarget().trim();
+        String wordExplain = word.getWordExplain().trim();
+        System.out.println(wordTarget);
+        System.out.println("- Meaning: " + wordExplain);
+        System.out.println();
     }
 
-    public static void main(String[] args) {
-        DictionaryCommandline newCommandLine = new DictionaryCommandline();
-        char query;
-        do {
-            System.out.println("*****Dictionary CommandLine*****");
-            System.out.println("0: Exit");
-            System.out.println("1: Dictionary Basic");
-            System.out.println("2: Dictionary Advanced");
-            System.out.print("Your action: ");
-            Scanner reader = new Scanner(System.in);
-            query = reader.next().charAt(0);
 
-            switch (query) {
-                case '0':
-                    System.out.println("Closing Dictionary CommandLine ...");
-                    break;
-                case '1':
-                    newCommandLine.dictionaryBasic();
-                    break;
-                case '2':
-                    newCommandLine.dictionaryAdvanced();
-                    break;
-                default:
-                    System.out.println("Action not supported.");
-                    break;
+    public void insertFromCommandLine() {
+        Scanner scanner = new Scanner(System.in);
+        int NumberOfWords = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print("Number of words: ");
+            try {
+                NumberOfWords = Integer.parseInt(scanner.nextLine());
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter an integer.");
             }
-        } while (query != '0');
+        }
+
+        for (int i = 0; i < NumberOfWords; i++) {
+            Word word = new Word();
+            System.out.println("Enter a new word: ");
+            word.setWordTarget(scanner.nextLine());
+            System.out.println("Enter meanings of the word: ");
+            word.setWordExplain(scanner.nextLine());
+            listWord.add(word);
+        }
     }
+
+
+    public void insertWordFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            String wordTarget = "";
+            StringBuilder wordExplain = new StringBuilder();
+            String wordPronunciation = "";
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.startsWith("@")) {
+                    wordTarget = line.substring(1).trim();
+                    int pronunciationStart = wordTarget.indexOf('/');
+                    if (pronunciationStart != -1) {
+                        int pronunciationEnd = wordTarget.lastIndexOf('/');
+                        if (pronunciationEnd > pronunciationStart) {
+                            wordPronunciation = wordTarget.substring(pronunciationStart + 1, pronunciationEnd).trim();
+                            wordTarget = wordTarget.substring(0, pronunciationStart).trim();
+                        }
+                    }
+                } else if (line.startsWith("*")) {
+                    String lineContents = line.substring(1).trim();
+                    wordExplain = new StringBuilder(lineContents);
+                } else if (line.startsWith("-")) {
+                    wordExplain.append(" ").append(line.substring(1).trim());
+                } else if (!line.isEmpty()) {
+                    Word word = new Word(wordTarget, wordExplain.toString(), wordPronunciation);
+                    listWord.add(word);
+                    wordTarget = "";
+                    wordExplain = new StringBuilder();
+                    wordPronunciation = "";
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void dictionaryLookup() {
+        Scanner scanner = new Scanner(System.in);
+        int query;
+        do {
+            System.out.print("Enter the word: ");
+            String wordTarget = scanner.nextLine();
+            Collections.sort(listWord);
+            int idx = Collections.binarySearch(listWord, new Word(wordTarget, null, null));
+            if (idx >= 0) {
+                System.out.println(listWord.get(idx).getWordPronunciation());
+                System.out.println(listWord.get(idx).getWordExplain());
+            } else {
+                System.out.println("The word does not exist in the dictionary.");
+            }
+
+            System.out.print("Do you want to continue to look up? Yes(1) No(2): ");
+            query = Integer.parseInt(scanner.nextLine());
+        } while (query == 1);
+    }
+        public static void exportToFile() {
+            File file = new File(OUTPUTFILEPATH);
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+                String format = "%-15s %-20s %-15s%n";
+                for (Word word : listWord) {
+                    bufferedWriter.write(String.format(format, word.getWordTarget(), word.getWordPronunciation(), word.getWordExplain()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    private static String formatMeanings(String wordExplain) {
+        String[] meanings = wordExplain.split("\\n");
+        StringBuilder formattedMeanings = new StringBuilder();
+        for (String meaning : meanings) {
+            formattedMeanings.append("-").append(meaning.trim()).append("\n");
+        }
+        return formattedMeanings.toString();
+    }
+
+    public void addWord() {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfWords = 0;
+        int option = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.println("Number of words: ");
+            String input = scanner.nextLine().trim();
+            if (input.matches("[0-9]+")) {
+                numberOfWords = Integer.parseInt(input);
+                validInput = true;
+            } else {
+                System.out.println("Invalid input. Please enter an positive integer.");
+                System.out.println("Continue to add word? Yes(y)/No(n)");
+                char query = scanner.nextLine().charAt(0);
+                if (query == 'n') {
+                    return;
+                }
+            }
+        }
+
+        for (int i = 0; i < numberOfWords; i++) {
+            while (true) {
+                System.out.println("Enter a new word: ");
+                String wordTarget = scanner.nextLine().trim().toLowerCase();
+                System.out.println("Enter pronunciation: ");
+                String wordPronunciation = scanner.nextLine().trim().toLowerCase();
+                System.out.println("Enter meanings of the word: ");
+                String wordExplain = scanner.nextLine().trim().toLowerCase();
+
+                if (!wordTarget.isEmpty() && !wordPronunciation.isEmpty() && !wordExplain.isEmpty()) {
+                    listWord.add(new Word(wordTarget, wordExplain, wordPronunciation));
+                    System.out.println("Add word successfully to the dictionary.");
+                    Collections.sort(listWord);
+                    break;
+                } else {
+                    System.err.println("Word, pronunciation, or explanation cannot be empty.");
+                    System.out.print("Continue? Yes(1) No(2): ");
+                    option = Integer.parseInt(scanner.nextLine().trim());
+                    if (option != 1) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public int binarySearchWordTarget(int start, int end, String wordTarget) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + ((end - start) / 2);
+        if (mid == 0 && listWord.isEmpty()) {
+            return -1;
+        }
+        int comp = wordTarget.compareTo(listWord.get(mid).getWordTarget());
+        if (comp == 0) {
+            return 0;
+        } else if (comp < 0) {
+            // Return the result of the recursive call
+            return binarySearchWordTarget(start, mid - 1, wordTarget);
+        } else {
+            // Return the result of the recursive call
+            return binarySearchWordTarget(mid + 1, end, wordTarget);
+        }
+    }
+
+    public void editWord() {
+        System.out.print("Enter the word to edit: ");
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+
+        boolean check = false;
+        for(Word w : listWord) {
+            if(w.getWordTarget().equalsIgnoreCase(word)) {
+                System.out.print("Enter the alternative word: ");
+                String target = scan.nextLine();
+                w.setWordTarget(target);
+
+                System.out.print("Enter the meaning of the alternative word: ");
+                String explain = scan.nextLine();
+                w.setWordExplain(explain);
+
+                check = true;
+                break;
+            }
+        }
+
+        if(!check) {
+            System.out.println("The word does not exist in the ");
+        }
+    }
+
+    public void dictionarySearcher() {
+        System.out.print("Enter the keyword to search: ");
+        Scanner scanner = new Scanner(System.in);
+        String keyword = scanner.nextLine().trim();
+
+        List<String> suggestions = new ArrayList<>();
+
+        for (Word word : listWord) {
+            String wordTarget = word.getWordTarget();
+            if (wordTarget.toLowerCase().startsWith(keyword.toLowerCase())) {
+                suggestions.add(wordTarget);
+            }
+        }
+
+        for (String suggestion : suggestions) {
+            System.out.println(suggestion);
+        }
+    }
+
+    public void removeWord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the word to remove: ");
+        String target = scanner.nextLine().trim().toLowerCase();
+
+        int idx = Collections.binarySearch(listWord, new Word(target, null));
+        if (idx >= 0) {
+            listWord.remove(listWord.get(idx));
+            System.out.println("Remove word successfully.");
+        } else {
+            System.out.println("The word does not exist in the dictionary. ");
+        }
+    }
+
+    /** done. */
+    public void modifyWord() {
+        System.out.println("Enter the word to edit: ");
+        Scanner scanner = new Scanner(System.in);
+        String target = scanner.nextLine().trim().toLowerCase();
+        String explain = scanner.nextLine().trim().toLowerCase();
+        String pronunciation = scanner.nextLine().trim().toLowerCase();
+
+        int idx = Collections.binarySearch(listWord, new Word(target, null));
+        if (idx >= 0) {
+            listWord.get(idx).setWordExplain(explain);
+            listWord.get(idx).setWordPronunciation(pronunciation);
+            System.out.println("The word has been removed successfully.");
+        } else {
+            System.out.println("The word does not exist in the dictionary.");
+        }
+    }
+
+    public void mcq() {
+        Scanner scanner = new Scanner(System.in);
+        int query;
+        int idx;
+        Random random = new Random();
+        QuizGame qg = new QuizGame();
+        qg.readquestionsfromFile();
+
+        // Set of valid options
+        Set<String> validOptions = Set.of("a", "b", "c", "d");
+
+        System.out.println(" ******RANDOM QUIZ GAME****** ");
+        System.out.println("  (Multiple-choice question)  ");
+        System.out.println("1.Start");
+        System.out.println("2.Return");
+        System.out.print("Your choice:");
+        query = Integer.parseInt(scanner.nextLine());
+        if (query == 2) {
+            return;
+        }
+
+        do {
+            idx = random.nextInt(qg.getSize());
+            QuizGame.Question currentQuestion = qg.getQuestions().get(idx);
+            System.out.println(currentQuestion.prompt());
+            System.out.println("Your choice (a, b, c, or d):");
+            String userAnswer = scanner.nextLine().toLowerCase();
+
+            // Check if the userAnswer is a valid option
+            if (validOptions.contains(userAnswer)) {
+                String correctAnswer = currentQuestion.answer().toLowerCase();
+
+                if (userAnswer.equals(correctAnswer)) {
+                    System.out.println("Correct answer.");
+                } else {
+                    System.out.println("Wrong answer. The correct answer is: " + correctAnswer);
+                }
+            } else {
+                System.out.println("Invalid choice. Please enter a, b, c, or d.");
+            }
+
+            System.out.print("Continue? Yes(1) No(2):");
+            query = Integer.parseInt(scanner.nextLine());
+        } while (query == 1);
+    }
+
 }
+
+
